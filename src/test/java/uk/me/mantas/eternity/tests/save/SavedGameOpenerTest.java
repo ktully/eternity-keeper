04cb5198129c04a773f0d8ccd6d7823f86826684
Fyorl <kim.mantas@gmail.com>
2015-07-23 12:04:21 +0100

    Modify tests to account for SaveGameOpener refactoring.

--- src/test/java/uk/me/mantas/eternity/tests/save/SavedGameOpenerTest.java
+++ src/test/java/uk/me/mantas/eternity/tests/save/SavedGameOpenerTest.java
@@ -20,6 +20,7 @@
 package uk.me.mantas.eternity.tests.save;
 
 import com.google.common.primitives.UnsignedInteger;
+import org.apache.commons.io.FileUtils;
 import org.cef.callback.CefQueryCallback;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -49,147 +50,6 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class SavedGameOpenerTest extends TestHarness {
-	private static final String EXTRACTED =
-		"[{\"isCompanion\":true"
-		+ ",\"stats\":{"
-			+ "\"StaminaRechargeMult\":1,\"LoreBonus\":0,\"FinishingBlowDamageMult\":1"
-			+ ",\"HostileAOEDamageMultiplier\":1,\"EngagementDistanceBonus\":0"
-			+ ",\"ConstitutionBonus\":0,\"FortitudeBonus\":0,\"KnockdownDefenseBonus\":0"
-			+ ",\"RangedAttackSpeedMultiplier\":1,\"AthleticsSkill\":0,\"ImmuneToEngagement\":false"
-			+ ",\"BaseMight\":16,\"TrapDamageOrDurationMult\":1,\"WhileKnockeddownDefenseBonus\":0"
-			+ ",\"VesselAccuracyBonus\":0,\"StunDurationMult\":1"
-			+ ",\"NearestAllyWithSharedTargetAccuracyBonus\":0,\"SurvivalSkill\":0"
-			+ ",\"WilderAccuracyBonus\":0,\"EnemyCritToHitPercent\":0"
-			+ ",\"OverrideSurvivalSkillLevel\":0,\"AoERadiusMult\":1,\"BonusMeleeDamage\":0"
-			+ ",\"NegMoveTickMult\":1,\"MeleeDTBypass\":0,\"PoisonDefenseBonus\":0"
-			+ ",\"HostileEffectDurationMultiplier\":1,\"RangedAccuracyBonus\":30"
-			+ ",\"DexterityBonus\":0,\"OverrideName\":\"Calisca\",\"KnockDownDurationMult\":1"
-			+ ",\"MeleeAccuracyBonus\":30,\"AttackSpeedMultiplier\":1,\"Experience\":0"
-			+ ",\"WeaponDamageMinMult\":1,\"m_LastLevelUpNotified\":0"
-			+ ",\"MeleeAttackSpeedMultiplier\":1,\"MaxStamina\":42,\"m_detectTimer\":0.70819074"
-			+ ",\"Level\":1,\"BonusRangedWeaponDamageMult\":1,\"StealthBonus\":0,\"DTBypass\":0"
-			+ ",\"ArmorSpeedFactorAdj\":0,\"BaseResolve\":11,\"RangedMovingRecoveryReductionPct\":0"
-			+ ",\"BonusMissToGrazePercent\":0,\"AthleticsBonus\":0,\"DeflectionBonus\":0"
-			+ ",\"LoreSkill\":0,\"TwoHandedDeflectionBonus\":0"
-			+ ",\"DistantEnemyWeaponAccuracyBonus\":0,\"DiseaseDefenseBonus\":0"
-			+ ",\"MeleeDamageRangePctIncreaseToMin\":0,\"BaseFortitude\":20"
-			+ ",\"EnemyHitToGrazePercent\":0,\"BonusMeleeDamageMult\":1,\"IntellectBonus\":0"
-			+ ",\"HealthStaminaPerLevel\":14,\"WoundDelay\":0,\"ReflexBonus\":0,\"BaseWill\":20"
-			+ ",\"ChanterPhraseRadiusMult\":1,\"EnemiesNeededToFlank\":2"
-			+ ",\"BonusHitToCritPercent\":0,\"ImmuneToEngageStop\":false"
-			+ ",\"m_redirectMeleeAttacks\":0,\"CraftingBonus\":0,\"BonusShieldDeflection\":0"
-			+ ",\"PerceptionBonus\":0,\"BaseIntellect\":12,\"StaminaRechargeBonus\":0"
-			+ ",\"AttackerToHitRollOverride\":-1,\"ExtraSimultaneousHitDefenseBonus\":0"
-			+ ",\"PotionEffectiveness\":1,\"BaseDeflection\":25,\"UnconsciousnessDelayed\":0"
-			+ ",\"BonusWeaponSets\":0,\"FortitudeWillGrazeToMissPercent\":0"
-			+ ",\"DisengagementAccuracyBonus\":0,\"DeathPrevented\":0,\"BonusHealMult\":1"
-			+ ",\"DisengagementDamageBonus\":0,\"StealthSkill\":0"
-			+ ",\"BonusTwoHandedMeleeWeaponDamageMult\":1,\"DamageMinBonus\":0"
-			+ ",\"BonusHitToCritPercentEnemyBelow10Percent\":0,\"PrimordialAccuracyBonus\":0"
-			+ ",\"BonusGrazeToMissPercent\":0,\"RateOfFireMultiplier\":1"
-			+ ",\"ClassHealthMultiplier\":5,\"CurrentGrimoireCooldown\":0,\"BonusDTFromArmor\":0"
-			+ ",\"DisengagementDefenseBonus\":0,\"FocusGainMult\":1,\"DOTTickMult\":1"
-			+ ",\"WillBonus\":0,\"WeaponSwitchCooldownBonus\":0,\"BaseConstitution\":14"
-			+ ",\"ProneDurationMult\":1,\"BonusQuickSlots\":0,\"EvadeEverything\":false"
-			+ ",\"FlankedAccuracyBonus\":0,\"BasePerception\":12,\"TrapAccuracyBonus\":0"
-			+ ",\"BonusGrazeToHitPercent\":0,\"MechanicsBonus\":0,\"StalkersLinkDamageMult\":1"
-			+ ",\"BaseDexterity\":10,\"DeflectReflexGrazeToMissPercent\":0,\"ResolveBonus\":0"
-			+ ",\"BonusHitToGrazePercent\":0,\"CraftingSkill\":0,\"MaxHealth\":42"
-			+ ",\"UnarmedAccuracyBonus\":0,\"SpellDefenseBonus\":0"
-			+ ",\"RangedAttackDistanceMultiplier\":1,\"ImprovedFlanking\":0"
-			+ ",\"WhileStunnedDefenseBonus\":0,\"BeastAccuracyBonus\":0,\"AOEDefenseBonus\":0"
-			+ ",\"DistantEnemyBonus\":0,\"RangedDeflectionBonus\":0,\"ReloadSpeedMultiplier\":1"
-			+ ",\"WildstrikeDamageMult\":1,\"ConcentrationBonus\":0"
-			+ ",\"ShieldDeflectExtendToReflex\":0,\"StaminaBonus\":0,\"DiseaseTickMult\":1"
-			+ ",\"GrimoireCooldownBonus\":0,\"SpellCastingDisabled\":0"
-			+ ",\"EnemyReflexGrazeToMissPercent\":0,\"BonusGrazeToHitPercentMeleeOneHanded\":0"
-			+ ",\"BonusMeleeWeaponDamageMult\":1,\"EngageableEnemyCount\":1"
-			+ ",\"BonusRangedWeaponCloseEnemyDamageMult\":1,\"StunDefenseBonus\":0"
-			+ ",\"InterruptMultiplier\":1,\"ZealousAuraRadiusMult\":1,\"VeilDeflectionBonus\":0"
-			+ ",\"BonusUnarmedDamage\":0,\"PushDefenseBonus\":0,\"BonusCritToHitPercent\":0"
-			+ ",\"RemainingSkillPoints\":0,\"MeleeAttackDistanceMultiplier\":1"
-			+ ",\"m_fatigueAccrued\":3600,\"MechanicsSkill\":0,\"MaxStaminaMultiplier\":1"
-			+ ",\"ExtraStraightBounces\":0,\"BonusArmorDTMultAtLowHealth\":1"
-			+ ",\"CritHitDamageMultiplierBonus\":0,\"RangedDTBypass\":0"
-			+ ",\"BonusUsesPerRestPastThree\":0,\"FrenzyDurationMult\":1,\"m_noiseTimer\":0"
-			+ ",\"PoisonTickMult\":1,\"SurvivalBonus\":0,\"m_recoveryTimer\":0"
-			+ ",\"DualWieldAttackSpeedMultiplier\":1,\"MightBonus\":0,\"BaseReflexes\":20"
-			+ ",\"CritHitDamageMultiplierBonusEnemyBelow10Percent\":0"
-		+ "},\"GUID\":\"b1a7e809-0000-0000-0000-000000000000\""
-		+ ",\"name\":\"Calisca\""
-		+ ",\"portrait\":\"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAACklEQVQI12P4DwABAQEAG7bu"
-			+ "VgAAAABJRU5ErkJggg==\""
-		+ ",\"isDead\":false"
-		+ "},{\"isCompanion\":false,\"stats\":{"
-			+ "\"StaminaRechargeMult\":1,\"LoreBonus\":0,\"FinishingBlowDamageMult\":1"
-			+ ",\"HostileAOEDamageMultiplier\":1,\"EngagementDistanceBonus\":0"
-			+ ",\"ConstitutionBonus\":0,\"FortitudeBonus\":0,\"KnockdownDefenseBonus\":0"
-			+ ",\"RangedAttackSpeedMultiplier\":1,\"AthleticsSkill\":0,\"ImmuneToEngagement\":false"
-			+ ",\"BaseMight\":16,\"TrapDamageOrDurationMult\":1,\"WhileKnockeddownDefenseBonus\":0"
-			+ ",\"VesselAccuracyBonus\":0,\"StunDurationMult\":1"
-			+ ",\"NearestAllyWithSharedTargetAccuracyBonus\":0,\"SurvivalSkill\":0"
-			+ ",\"WilderAccuracyBonus\":0,\"EnemyCritToHitPercent\":0"
-			+ ",\"OverrideSurvivalSkillLevel\":0,\"AoERadiusMult\":1,\"BonusMeleeDamage\":0"
-			+ ",\"NegMoveTickMult\":1,\"MeleeDTBypass\":0,\"PoisonDefenseBonus\":0"
-			+ ",\"HostileEffectDurationMultiplier\":1,\"RangedAccuracyBonus\":30"
-			+ ",\"DexterityBonus\":0,\"OverrideName\":\"Elenor\",\"KnockDownDurationMult\":1"
-			+ ",\"MeleeAccuracyBonus\":30,\"AttackSpeedMultiplier\":1,\"Experience\":0"
-			+ ",\"WeaponDamageMinMult\":1,\"m_LastLevelUpNotified\":0"
-			+ ",\"MeleeAttackSpeedMultiplier\":1,\"MaxStamina\":42,\"m_detectTimer\":0.41699225"
-			+ ",\"Level\":1,\"BonusRangedWeaponDamageMult\":1,\"StealthBonus\":0,\"DTBypass\":0"
-			+ ",\"ArmorSpeedFactorAdj\":0,\"BaseResolve\":10,\"RangedMovingRecoveryReductionPct\":0"
-			+ ",\"BonusMissToGrazePercent\":0,\"AthleticsBonus\":0,\"DeflectionBonus\":0"
-			+ ",\"LoreSkill\":0,\"TwoHandedDeflectionBonus\":0"
-			+ ",\"DistantEnemyWeaponAccuracyBonus\":0,\"DiseaseDefenseBonus\":0"
-			+ ",\"MeleeDamageRangePctIncreaseToMin\":0,\"BaseFortitude\":20"
-			+ ",\"EnemyHitToGrazePercent\":0,\"BonusMeleeDamageMult\":1,\"IntellectBonus\":0"
-			+ ",\"HealthStaminaPerLevel\":14,\"WoundDelay\":0,\"ReflexBonus\":0,\"BaseWill\":20"
-			+ ",\"ChanterPhraseRadiusMult\":1,\"EnemiesNeededToFlank\":2"
-			+ ",\"BonusHitToCritPercent\":0,\"ImmuneToEngageStop\":false"
-			+ ",\"m_redirectMeleeAttacks\":0,\"CraftingBonus\":0,\"BonusShieldDeflection\":0"
-			+ ",\"PerceptionBonus\":0,\"BaseIntellect\":10,\"StaminaRechargeBonus\":0"
-			+ ",\"AttackerToHitRollOverride\":-1,\"ExtraSimultaneousHitDefenseBonus\":0"
-			+ ",\"PotionEffectiveness\":1,\"BaseDeflection\":25,\"UnconsciousnessDelayed\":0"
-			+ ",\"BonusWeaponSets\":0,\"FortitudeWillGrazeToMissPercent\":0"
-			+ ",\"DisengagementAccuracyBonus\":0,\"DeathPrevented\":0,\"BonusHealMult\":1"
-			+ ",\"DisengagementDamageBonus\":0,\"StealthSkill\":0"
-			+ ",\"BonusTwoHandedMeleeWeaponDamageMult\":1,\"DamageMinBonus\":0"
-			+ ",\"BonusHitToCritPercentEnemyBelow10Percent\":0,\"PrimordialAccuracyBonus\":0"
-			+ ",\"BonusGrazeToMissPercent\":0,\"RateOfFireMultiplier\":1"
-			+ ",\"ClassHealthMultiplier\":5,\"CurrentGrimoireCooldown\":0,\"BonusDTFromArmor\":0"
-			+ ",\"DisengagementDefenseBonus\":0,\"FocusGainMult\":1,\"DOTTickMult\":1"
-			+ ",\"WillBonus\":0,\"WeaponSwitchCooldownBonus\":0,\"BaseConstitution\":12"
-			+ ",\"ProneDurationMult\":1,\"BonusQuickSlots\":0,\"EvadeEverything\":false"
-			+ ",\"FlankedAccuracyBonus\":0,\"BasePerception\":14,\"TrapAccuracyBonus\":0"
-			+ ",\"BonusGrazeToHitPercent\":0,\"MechanicsBonus\":0,\"StalkersLinkDamageMult\":1"
-			+ ",\"BaseDexterity\":13,\"DeflectReflexGrazeToMissPercent\":0,\"ResolveBonus\":0"
-			+ ",\"BonusHitToGrazePercent\":0,\"CraftingSkill\":5,\"MaxHealth\":42"
-			+ ",\"UnarmedAccuracyBonus\":0,\"SpellDefenseBonus\":0"
-			+ ",\"RangedAttackDistanceMultiplier\":1,\"ImprovedFlanking\":0"
-			+ ",\"WhileStunnedDefenseBonus\":0,\"BeastAccuracyBonus\":0,\"AOEDefenseBonus\":0"
-			+ ",\"DistantEnemyBonus\":0,\"RangedDeflectionBonus\":0,\"ReloadSpeedMultiplier\":1"
-			+ ",\"WildstrikeDamageMult\":1,\"ConcentrationBonus\":0"
-			+ ",\"ShieldDeflectExtendToReflex\":0,\"StaminaBonus\":0,\"DiseaseTickMult\":1"
-			+ ",\"GrimoireCooldownBonus\":0,\"SpellCastingDisabled\":0"
-			+ ",\"EnemyReflexGrazeToMissPercent\":0,\"BonusGrazeToHitPercentMeleeOneHanded\":0"
-			+ ",\"BonusMeleeWeaponDamageMult\":1,\"EngageableEnemyCount\":1"
-			+ ",\"BonusRangedWeaponCloseEnemyDamageMult\":1,\"StunDefenseBonus\":0"
-			+ ",\"InterruptMultiplier\":1,\"ZealousAuraRadiusMult\":1,\"VeilDeflectionBonus\":0"
-			+ ",\"BonusUnarmedDamage\":0,\"PushDefenseBonus\":0,\"BonusCritToHitPercent\":0"
-			+ ",\"RemainingSkillPoints\":0,\"MeleeAttackDistanceMultiplier\":1"
-			+ ",\"m_fatigueAccrued\":0,\"MechanicsSkill\":0,\"MaxStaminaMultiplier\":1"
-			+ ",\"ExtraStraightBounces\":0,\"BonusArmorDTMultAtLowHealth\":1"
-			+ ",\"CritHitDamageMultiplierBonus\":0,\"RangedDTBypass\":0"
-			+ ",\"BonusUsesPerRestPastThree\":0,\"FrenzyDurationMult\":1,\"m_noiseTimer\":0"
-			+ ",\"PoisonTickMult\":1,\"SurvivalBonus\":0,\"m_recoveryTimer\":-0.004099274"
-			+ ",\"DualWieldAttackSpeedMultiplier\":1,\"MightBonus\":0,\"BaseReflexes\":20"
-			+ ",\"CritHitDamageMultiplierBonusEnemyBelow10Percent\":0"
-		+ "},\"GUID\":\"09517a0d-4fec-407c-a749-a531f3be64e0\""
-		+ ",\"name\":\"Elenor\""
-		+ ",\"portrait\":\"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAACklEQVQI12P4DwABAQEAG7bu"
-			+ "VgAAAABJRU5ErkJggg==\""
-		+ ",\"isDead\":false}]";
-
 	private static final String DESERIALIZATION_ERR = "{\"error\":\"DESERIALIZATION_ERR\"}";
 
 	@Test
@@ -218,20 +78,27 @@ public class SavedGameOpenerTest extends TestHarness {
 	}
 
 	@Test
-	public void saveGameOpened () throws URISyntaxException {
-		Settings mockSettings = mockSettings();
-		JSONObject mockJSON = mock(JSONObject.class);
-		CefQueryCallback mockCallback = mock(CefQueryCallback.class);
-		File resources = new File(getClass().getResource("/").toURI());
-		SavedGameOpener cls =
+	public void saveGameOpened () throws URISyntaxException, IOException {
+		final Settings mockSettings = mockSettings();
+		final JSONObject mockJSON = mock(JSONObject.class);
+		final CefQueryCallback mockCallback = mock(CefQueryCallback.class);
+		final File resources = new File(getClass().getResource("/").toURI());
+		final SavedGameOpener cls =
 			new SavedGameOpener(resources.getAbsolutePath(), mockCallback);
+
+		final File extractedFile =
+			new File(getClass().getResource("/SavedGameOpenerExtracted.json").toURI());
+
+		final String extractedJSONString = FileUtils.readFileToString(extractedFile);
+		// We stick the JSON string in a JSON object to remove all whitespace.
+		final JSONObject extractedJSON = new JSONObject(extractedJSONString);
 
 		mockSettings.json = mockJSON;
 		when(mockJSON.getString("gameLocation")).thenReturn(
 			new File(resources, "SavedGameOpenerTest").getAbsolutePath());
 
 		cls.run();
-		verify(mockCallback).success(EXTRACTED);
+		verify(mockCallback).success(extractedJSON.toString());
 	}
 
 	private PacketDeserializer mockDeserializer (final Environment mockEnvironment) {
@@ -282,25 +149,15 @@ public class SavedGameOpenerTest extends TestHarness {
 	@SuppressWarnings("unchecked")
 	public void extractCharactersTest () {
 		final CefQueryCallback mockCallback = mock(CefQueryCallback.class);
-		final Property notObjPersistencePacket = mock(Property.class);
-		final Property noObjectName = mock(Property.class);
 		final Property noObjectID = mock(Property.class);
 		final Property startsWithCompanion = mock(Property.class);
 		final Property startsWithPlayer = mock(Property.class);
 
 		final List<Property> gameObjects = new ArrayList<Property>(){{
-			add(notObjPersistencePacket);
-			add(noObjectName);
 			add(noObjectID);
 			add(startsWithCompanion);
 			add(startsWithPlayer);
 		}};
-
-		notObjPersistencePacket.obj = new Object();
-
-		final ObjectPersistencePacket noObjectNamePacket = new ObjectPersistencePacket();
-		noObjectNamePacket.ObjectName = null;
-		noObjectName.obj = noObjectNamePacket;
 
 		final ObjectPersistencePacket noObjectIDPacket = new ObjectPersistencePacket();
 		noObjectIDPacket.ObjectName = "";
