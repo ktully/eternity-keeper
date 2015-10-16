70573342762116f162b607ae6fdf5684ba4da23e
Fyorl <kim.mantas@gmail.com>
2015-10-16 15:30:11 +0100

    Updated launch script for linux.

--- eternity.sh
+++ eternity.sh
@@ -1,34 +1,65 @@
 #!/bin/sh
 
-ROOT=`pwd`
+ROOT=$(dirname $(readlink -f $0))
 JRE="$ROOT/jre/bin/java"
 JARS="$ROOT/jar"
-ZIPS="$ROOT/src"
+SRC="$ROOT/src"
 
+# Detect an update.
 max=-1
-for j in `ls $JARS`; do
-	timestamp="${j%.*}"
-	if [ $timestamp -gt $max ]; then
-		max=$timestamp
-	fi
-done
-
-JAR="$JARS/${max}.jar"
-
-max=-1
-for z in `ls $ZIPS`; do
-	timestamp="${z%.*}"
+for f in `find "$ROOT" -iname "*.zip"`; do
+	zip=`basename "$f"`
+	timestamp="${zip%.*}" # Remove the extension.
 	if [ $timestamp -gt $max ]; then
 		max=$timestamp
 	fi
 done
 
 if [ $max -gt -1 ]; then
-	ZIP="$ZIPS/${max}.zip"
-	mv "$ZIPS/ui" "$ZIPS/ui.bak"
-	unzip $ZIP -d $ZIPS
-	rm -rf "$ZIPS/ui.bak"
-	rm $ZIPS/*.zip
+	# Clean up any old update directory.
+	if [ -d "$ROOT/update" ]; then
+		rm -rf "$ROOT/update"
+	fi
+
+	mkdir "$ROOT/update"
+	unzip "$ROOT/${max}.zip" -d "$ROOT/update"
+
+	# There should only be one directory in the zip.
+	UPDATE=`ls "$ROOT/update/"`
+	UPDATE="$ROOT/update/$UPDATE"
+
+	# Remove the old backups.
+	if [ -d "$ROOT/jar.old" ]; then
+		rm -rf "$ROOT/jar.old"
+	fi
+
+	if [ -d "$ROOT/src.old" ]; then
+		rm -rf "$ROOT/src.old"
+	fi
+
+	# Create new backups.
+	mv "$JARS" "$ROOT/jar.old"
+	mv "$SRC" "$ROOT/src.old"
+
+	# Remove the now backed-up directories.
+	rm -rf "$JARS"
+	rm -rf "$SRC"
+
+	mkdir "$JARS"
+	mkdir "$SRC"
+
+	# Copy the updates.
+	cp "$UPDATE/*.jar" "$JARS"
+	cp -r "$UPDATE/ui" "$SRC"
+
+	# Clean up any left over zips.
+	rm -f "$ROOT/*.zip"
 fi
 
-"$JRE" -jar -Djava.library.path=lib "$JAR"
+LIB="$ROOT/lib/native/linux64"
+export LD_LIBRARY_PATH=$LIB
+
+LD_PRELOAD="$LIB/libcef.so" "$JRE" \
+	-Djava.library.path="$LIB" \
+	-cp "$JARS/*" \
+	uk.me.mantas.eternity.EternityKeeper
